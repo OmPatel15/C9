a919055b44869dae3728ab4b1257944b50e67d29
OmPatel15 <108162110+OmPatel15@users.noreply.github.com>
2022-06-25 11:08:31 +0930

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var ball;
+function setup(){
+  createCanvas(400,400);
+  ball=createSprite(200,200,20,20);
+}
+function draw(){
+  background("blue");
+  if(keyDown(RIGHT_ARROW)){
+    ball.x=ball.x+5;
+  }
+  if(keyDown(LEFT_ARROW)){
+    ball.x=ball.x-5;
+  }
+  if(keyDown(UP_ARROW)){
+    ball.y=ball.y-5;
+  }
+  if(keyDown(DOWN_ARROW)){
+    ball.y=ball.y+5;
+  }
+drawSprites();
+}
